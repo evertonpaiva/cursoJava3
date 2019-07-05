fc43885e175b674e9b55c5550e728c667cbcae44
Everton Paiva <evertonpaiva@gmail.com>
2019-07-05 12:30:41 -0300

    Utilizando herança

--- bytebank-herdado/src/Funcionario.java
+++ bytebank-herdado/src/Funcionario.java
@@ -5,10 +5,6 @@ public class Funcionario {
 	private String cpf;
 	private double salario;
 	
-	public Funcionario() {
-		
-	}
-	
 	public double getBonificacao() {
 		return this.salario * 0.1;
 	}
