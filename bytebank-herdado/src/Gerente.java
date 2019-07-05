ef1cf60cccbc2f49b10c0a7e14175459d0f009a1
Everton Paiva <evertonpaiva@gmail.com>
2019-07-05 16:04:02 -0300

    Criando uma classe abstrata intermediária

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,19 +1,5 @@
 
-public class Gerente extends Funcionario {
-
-	private int senha;
-	
-	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
+public class Gerente extends FuncionarioAutenticavel {	
 	
 	public double getBonificacao() {
 		System.out.println("Bonificacao do gerente");
