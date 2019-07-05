f0e6a2014db4f0e10f960ca88c997f6e88ac794e
Everton Paiva <evertonpaiva@gmail.com>
2019-07-05 14:42:38 -0300

    Sobrescrevendo uma função herdada.

--- bytebank-herdado-conta/src/Conta.java
+++ bytebank-herdado-conta/src/Conta.java
@@ -11,8 +11,8 @@ public class Conta {
         System.out.println("O total de contas é " + Conta.total);
         this.agencia = agencia;
         this.numero = numero;
-        this.saldo = 100;
-        System.out.println("Estou criando uma conta " + this.numero);
+        //this.saldo = 100;
+        //System.out.println("Estou criando uma conta " + this.numero);
     }
 
     public void deposita(double valor) {
